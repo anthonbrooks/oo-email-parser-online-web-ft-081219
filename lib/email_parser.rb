3447756f26b8bbcd7ca5cc3ecf5c5a275a3d3170
Anthony Prescott-Brooks <anthonybrooks226@gmail.com>
2019-08-24 00:05:28 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -5,10 +5,12 @@
 
 class EmailAddressParser
   
-  attr_accessor
+  attr_accessor :emails
   
-  @@emails = []
-  
+  def initialize(emails)
+    @emails = emails
+  end
+
   def parse(emails)
     emails.split('/[/s,]/')
   end
